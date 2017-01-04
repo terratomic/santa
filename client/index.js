6472d804874cb87f757783a9fe4058e3856ecfd3
Pooja Welling <pgwelling@gmail.com>
2017-01-04 00:29:52 -0500

    so much for a footer. new solution needed

--- client/index.js
+++ client/index.js
@@ -14,10 +14,13 @@ function addDivs(){
   "<input class = \"field\" type=\"text\" name = \"name\"/>"+
 "<input class = \"field\" type = \"text\" name = \"email\" />"+
   "<br />";
+  
   document.getElementById('person').innerHTML+=dummy;
 }
 
 $( document ).ready(function() {
   console.log( "ready!" );
   document.getElementById("add").onclick = addDivs;
+/*  document.getElementById("footer").style.position = "absolute";
+  document.getElementById("footer").style.bottom = "0";*/
 });

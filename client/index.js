75348baceb2c9419ae91d91f9f28b4bfba5814f7
Pooja Welling <pgwelling@gmail.com>
2017-01-03 23:24:52 -0500

    adding divs functioning. scrolling to be implemented

--- client/index.js
+++ client/index.js
@@ -1,57 +1,11 @@
-/*
-var express = require('express');
-var app = express();
-
-app.use(express.bodyParser());
-
-app.set('port', (process.env.PORT || 8000));
-
-app.use(express.static(__dirname + '/client'));
-
-app.post('form', function(request, response){
-    console.log(request.query.name);
-    console.log(request.query.email);
-});
-
-app.get('/', function(request, response) {
-    response.render('client/index')
-});
-
-
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
-*/
-//-----------------------------------------
-/*
-$( document ).ready(function() {
-  console.log( "ready!" );
-  document.getElementById("submit").onclick = submit;
-});
-
-function submit(){
-  var http = require('http');
-
-  var option = {
-      hostname : "localhost" ,
-      port : 8000 ,
-      method : "POST",
-      path : "/"
-  }
-
-      var request = http.request(option , function(resp){
-         resp.on("data",function(chunck){
-             console.log(chunck.toString());
-         })
-      })
-      request.end();
-
-  $( "#submit" ).click(function() {
-    $.post('../server.js', function(data){
-      alert("Data: " + data);
-    });
-  });
-
+function addDivs(){
+  var dummy = "<input class = \"field\" type=\"text\" name = \"name\"/>"+
+"<input class = \"field\" type = \"text\" name = \"email\" />"+
+  "<br />";
+  document.getElementById('person').innerHTML+=dummy;
 }
 
-*/
+$( document ).ready(function() {
+  console.log( "ready!" );
+  document.getElementById("add").onclick = addDivs;
+});
